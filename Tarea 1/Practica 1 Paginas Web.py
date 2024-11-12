466ec43fe29feb72206194f82247cf93b21c9a8b
Elthon Pessoa Piña <elthon.pessoa30@gmail.com>
2024-11-12 13:55:45 -0600

    Tarea 1 Semana 1
    El primer código consiste en ayudar a alguien a saber cuanto dinero gana al crear paginas web.
    
    El segundo código consiste en un calculo de mortalidad de un país "x" con la información de la cantidad de enfermos y muertos

---
+++ Tarea 1/Practica 1 Paginas Web.py
@@ -0,0 +1,3 @@
+Paginas= int(input("Digite la cantida de paginas que recibio este mes:"))
+Ganancias= Paginas*50
+print("La ganancia que tuvo este mes por las paginas hechas es de:")
---
+++ Tarea 1/Practica 2 Covid 19.py
@@ -0,0 +1,5 @@
+pais= str(input("¿Digite el nombre del pais?"))
+enfer=int(input("¿Cual es la cantidad de enfermos?:"))
+dies=int(input("¿Cual es la cantidad de muiertos?:"))
+mortalidad= dies/1000
+print("En el pais de",pais,"Por cada 1000 habitantes las tasa de mortalida es de:", mortalidad)
